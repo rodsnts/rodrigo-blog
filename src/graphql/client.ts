c2b162797595786026a73d05aa7db79525f27ace
Rodrigo Santos <rr.santos1232@gmail.com>
2022-04-02 17:48:01 +0100

    FIX: graphql endpoint env var

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-const client = new GraphQLClient(`http://localhost:1337/graphql`);
+const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_GRAPH_ENDPOINT}`);
 
 client.setHeader("Authorization", `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`);
 
